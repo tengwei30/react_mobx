64b9704f01555c0c46639874f3454d8eded8cf40
zhangsong <zhangsong@guanghe.tv>
2018-01-29 21:50:16 +0800

    comment: meeting

--- src/pages/Comment/chart.jsx
+++ src/pages/Comment/chart.jsx
@@ -3,6 +3,7 @@ import times from '../../common/timeConfig.js';
 import './comment.css';
 import { autorun } from 'mobx';
 import { inject, observer } from 'mobx-react';
+import moment from 'moment';
 
 @inject('commentStore', 'layoutStore')
 @observer
@@ -18,7 +19,6 @@ export default class Chart extends React.Component {
     }
     render() {
         const {commentStore} = this.props
-        console.log(commentStore)
         return(
             <div className="Chart">
                 <div className="CtContent">
@@ -37,10 +37,118 @@ export default class Chart extends React.Component {
                             {
                                 commentStore.switchWeek ? (
                                     commentStore.columnData.slice(0, 7).map(item => {
-                                       console.log(item)
+                                       return (
+                                           <div className="weekday" key={item.day}>
+                                                {
+                                                    (moment().format('YYYY-MM-DD') === item.day) ? (
+                                                        <div className="weekdayHeader">
+                                                            <span style={{color:'rgb(112,157,228)'}}>{ item.day }</span>
+                                                            <span style={{color:'rgb(112,157,228)'}}>{ item.week }</span>
+                                                        </div>
+                                                    ) : (
+                                                        <div className="weekdayHeader">
+                                                            <span>{ item.day }</span>
+                                                            <span>{ item.week }</span>
+                                                        </div>
+                                                    )
+                                                }
+                                                {
+                                                    item['times'].map((val, key) => {
+                                                        if(val.used) {
+                                                            return (
+                                                                <div key={val.time} className="timeSingleBlock">
+                                                                    <div className="create"
+                                                                    // onClick={() => {this.showCreateRoom(item,val)}}
+                                                                    >
+                                                                        {
+                                                                            (val.time == val.beginTime) ? (
+                                                                                <div className="active" style={{borderTop:'2px solid #fff'}}>
+                                                                                <span className="description">{val.description}</span>
+                                                                                </div>
+                                                                            ) : (
+                                                                                <div className="active">
+                                                                                    <span>.</span>
+                                                                                </div>
+                                                                            )
+                                                                        }
+                                                                    </div>
+                                                                </div>
+                                                            )
+                                                        } else {
+                                                            return (
+                                                                <div key={val.time} className="timeSingleBlock">
+                                                                    <div className="create"
+                                                                    // onClick={() => {this.showCreateRoom(item,val)}}
+                                                                    >
+                                                                        <div className="makeMeet">
+                                                                            预定
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            )
+                                                        }
+                                                    })
+                                                }
+                                           </div>
+                                       )
                                     })
                                 ) : (
-                                    console.log(2)
+                                    commentStore.columnData.slice(7, 14).map(item => {
+                                        return (
+                                            <div className="weekday" key={item.day}>
+                                                 {
+                                                     (moment().format('YYYY-MM-DD') === item.day) ? (
+                                                         <div className="weekdayHeader">
+                                                             <span style={{color:'rgb(112,157,228)'}}>{ item.day }</span>
+                                                             <span style={{color:'rgb(112,157,228)'}}>{ item.week }</span>
+                                                         </div>
+                                                     ) : (
+                                                         <div className="weekdayHeader">
+                                                             <span>{ item.day }</span>
+                                                             <span>{ item.week }</span>
+                                                         </div>
+                                                     )
+                                                 }
+                                                 {
+                                                     item['times'].map((val, key) => {
+                                                         if(val.used) {
+                                                             return (
+                                                                 <div key={val.time} className="timeSingleBlock">
+                                                                     <div className="create"
+                                                                     // onClick={() => {this.showCreateRoom(item,val)}}
+                                                                     >
+                                                                         {
+                                                                             (val.time == val.beginTime) ? (
+                                                                                 <div className="active" style={{borderTop:'2px solid #fff'}}>
+                                                                                 <span className="description">{val.description}</span>
+                                                                                 </div>
+                                                                             ) : (
+                                                                                 <div className="active">
+                                                                                     <span>.</span>
+                                                                                 </div>
+                                                                             )
+                                                                         }
+                                                                     </div>
+                                                                 </div>
+                                                             )
+                                                         } else {
+                                                             return (
+                                                                 <div key={val.time} className="timeSingleBlock">
+                                                                     <div className="create"
+                                                                     // onClick={() => {this.showCreateRoom(item,val)}}
+                                                                     >
+                                                                         <div className="makeMeet">
+                                                                             预定
+                                                                         </div>
+                                                                     </div>
+                                                                 </div>
+                                                             )
+                                                         }
+                                                     })
+                                                 }
+                                            </div>
+                                        )
+                                     })
                                 )
                             }
                         </div>
--- src/pages/Comment/comment.jsx
+++ src/pages/Comment/comment.jsx
@@ -3,8 +3,14 @@ import { Button } from 'antd';
 import moment from 'moment';
 import './comment.css';
 import Chart from './chart.jsx';
+import { inject, observer } from 'mobx-react';
 
+@inject('commentStore')
+@observer
 export default class Comment extends React.Component {
+    switchWeek = () => {
+        this.props.commentStore.setswitchWeek(!this.props.commentStore.switchWeek)
+    }
     render() {
         return(
             <div style={{width:'100%',height:'100%'}}>
@@ -17,7 +23,7 @@ export default class Comment extends React.Component {
         return(
             <div className="btnDate">
                 <p>今天：{moment().locale('zh-cn').format('dddd')}</p>
-                <p><Button type="primary">本周</Button></p>
+                <p><Button type="primary" onClick={this.switchWeek}>{this.props.commentStore.switchWeek ? '本周' : '下周' }</Button></p>
             </div>
         )
     }
